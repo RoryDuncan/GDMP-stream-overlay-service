5995aca60f4b7ca829f3767ad685228546e4c0b9
Rory Duncan <robust.rory@gmail.com>
2017-10-17 09:22:47 -0700

    Render data for song

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 const path = require("path");
 const chokidar = require('chokidar');
 const socketIO = require('socket.io');
-
+const jsonfile = require('jsonfile')
 
 // helper
 const throttle = (ms, fn) => {
@@ -32,21 +32,39 @@ fs.access(file, fs.constants.R_OK | fs.constants.W_OK, (err) => {
 
 if (!canAccess) throw new Error("No read or write access to playback.json file!");
 
-const watcher = chokidar.watch(file, {
-	persistant: true,
-});
-
-watcher.on("change", throttle(1000, (path, stats) => {
-	console.log(`${path} was changed.`)
-}));
 
 
 
 module.exports = (server) => {
 
+	let connected = false;
 	let io = socketIO(server);
 
+	let watcher = chokidar.watch(file, {
+		persistant: true,
+	});
+
+	watcher.on("change", throttle(1000, (path, stats) => {
+		console.log(`${path} has changed.`);
+		if (connected) {
+			jsonfile.readFile(path, function(err, data) {
+				if (err) {
+					console.error("Error parsing JSON:", err);
+				}
+				else {
+		 			io.emit("update", data);
+				}
+			})
+		}
+		
+	}));
+
+
 	io.on('connection', (client) => {
+		connected = true;
 		console.log("client connected!")
+		client.on("disconnect", () => {
+			connected = false;
+		})
 	});
 }
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,10 +2,37 @@
 
 	console.log("hey")
 
+
+	const template = function(data){
+		return `
+			<div class="media-player">
+				<div class="song-title">${data.song.title}</div>
+				<div class="album-title">${data.song.album}</div>
+				<div class="artist">${data.song.artist}</div>
+				<div class="album-art">
+					<img src="${data.song.albumArt}" alt="${data.song.album}" />
+				</div>
+			</div>
+		`
+
+	}
+
+
+	const render = (data) => {
+		let root = document.getElementById("media-player-root")
+		if (root != null) {
+			let html = template(data);
+			console.log("rendering", root)
+			root.innerHTML = html;
+		}
+	}
+
+
 	var socket = io();
 
 	socket.on("update", (data) => {
 		console.log("data:", data)
+		window.requestAnimationFrame(() => render(data));
 	})
 
 })()
